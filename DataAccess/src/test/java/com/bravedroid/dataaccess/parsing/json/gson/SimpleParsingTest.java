ad3ba113a336e26eeab3b182997cd6618e34f596
refka-farid <refka.farid@gmail.com>
2018-11-10 17:37:08 +0100

    [FIX] substituted the quotation marks with apostrophes to avoid tons of Java escaping.

--- DataAccess/src/test/java/com/bravedroid/dataaccess/parsing/json/gson/SimpleParsingTest.java
+++ DataAccess/src/test/java/com/bravedroid/dataaccess/parsing/json/gson/SimpleParsingTest.java
@@ -19,13 +19,13 @@ public class SimpleParsingTest {
     public void toJsonStringTest() {
         User user = new User(50, "Mike", "Tomas");
         String userString = SUT.toJsonString(user);
-        String expected = "{\"age\":50,\"firstName\":\"Mike\",\"lastName\":\"Tomas\"}";
-        assertEquals(expected, userString);
+        String expected = "{'age':50,'firstName':'Mike','lastName':'Tomas'}";
+        assertEquals(expected.replaceAll("'", "\""), userString);
     }
 
     @Test
     public void toObjectTest() {
-        String userString = "{\"age\":50,\"firstName\":\"Mike\",\"lastName\":\"Tomas\"}";
+        String userString = "{'age':50,'firstName':'Mike','lastName':'Tomas'}";
         User expectedUser = new User(50, "Mike", "Tomas");
         User user = SUT.toObject(userString);
         assertEquals(expectedUser, user);
